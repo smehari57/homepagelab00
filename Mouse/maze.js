681a1d64983214ef225620eb555d3a82d51aa438
smehari57 <starmehari5@gmail.com>
2022-01-23 20:03:58 -0600

    My maze

---
+++ Mouse/maze.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function () {
+
+  let flag = false;
+
+  $("#start").click(function () {
+    $("#status").text('Click the "S" to begin');
+
+    if (!flag) {
+      $("div .boundary").mouseenter(
+        function(){
+          lostAction();
+          flag = true;
+        });
+      $("#maze").mouseleave(
+        function(){
+          lostAction();
+          flag = true;
+        });
+    }
+    else {
+      $("div .boundary").removeClass("youlose");
+      $("#end").mouseenter(function () {
+        winAction();
+        flag = false;
+
+      })
+    }
+
+  })
+
+});
+
+function lostAction(){
+  $("div .boundary").addClass("youlose");
+  $("#status").text("You Lose! :(");
+  $("#end").unbind("mouseenter");
+}
+
+function winAction(){
+  $("#status").text("You Win! :)");
+  $("div .boundary").unbind("mouseenter");
+  $("#maze").unbind("mouseleave");
+}
